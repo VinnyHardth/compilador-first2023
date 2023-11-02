15d8458d8d6f2db2e60bca2310af69f26c2d2df2
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2023-11-01 21:37:38 -0400

    ajustando .sh para retirar arquivos temporários

--- GCI/run.sh
+++ GCI/run.sh
@@ -1,11 +1,17 @@
+function cleanup() {
+  # Função para remover os arquivos gerados automaticamente
+  rm -f y.tab.c y.tab.h lex.yy.c a.out
+}
+
+# Registrar a função de limpeza para ser executada em caso de falha ou saída do script
+trap cleanup EXIT
+
 function executar_teste_unico() {
   flex scanner.l && bison -o y.tab.c -d parser.y && gcc y.tab.c lex.yy.c -w
   local nome_arquivo="$1"
   if [ -f "$nome_arquivo" ]; then
     echo "Executando: $nome_arquivo"
     ./a.out "$nome_arquivo"
-    # Remover os arquivos gerados automaticamente após a execução
-    rm -f y.tab.c y.tab.h lex.yy.c a.out
   else
     echo "Arquivo de caso de teste não encontrado: $nome_arquivo"
   fi
